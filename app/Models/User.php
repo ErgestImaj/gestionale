301affaf410a5a2b1bcb00b8245bcbb7ab5eb4ef
evolve-al <info@evolve-al.com>
2019-10-01 11:34:52 +0200

    Soft delete for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -58,7 +59,6 @@ class User extends Authenticatable
             }
             $item->username = $uid;
             $item->slug = strtolower($uid);
-
         });
     }
 
