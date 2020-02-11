34dfa2bb73cacbab1256759edebb4d71c9c9859d
mage_2018 <ergest.imaj@magellano.it>
2020-02-11 23:37:55 +0100

    Add status model

--- app/Models/Structure.php
+++ app/Models/Structure.php
@@ -12,12 +12,15 @@ class Structure extends Model
 {
 	use HasUserRelationships, HasHashid, HashidRouting, HashIdAttribute;
 
-	const CREATED_AT = 'created';
-	const UPDATED_AT = 'updated';
-	protected $table = 'structures_structures';
-	protected $guarded = [];
-	protected $appends = [
-		'hashid'
-	];
+    const CREATED_AT = 'created';
+    const UPDATED_AT = 'updated';
+    protected $table = 'structures_structures';
+    protected $guarded = [];
+    protected $appends = [
+        'hashid'
+    ];
 
+	public function status(){
+	    return $this->hasOne(StructureStatus::class,'structure_id');
+    }
 }
