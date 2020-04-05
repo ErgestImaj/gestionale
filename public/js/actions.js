e471a761d00f3be13329625216d2d838665e7cc1
Ergest Imaj <ergest.imaj@gmail.com>
2020-04-05 20:31:24 +0200

    Add public js

---
+++ public/js/actions.js
@@ -0,0 +1,124 @@
+$(document).on('click', '.delete-btn', function (e) {
+	e.preventDefault();
+	let actionurl = $(this).data('action');
+	let msg = $(this).data('content');
+	swal({
+		title: "Sei sicuro?",
+		text: msg,
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+	}).then((willDelete) => {
+		if (willDelete) {
+			axios.delete(actionurl)
+				.then(response => {
+					if (response.data.status == 'success') {
+						swal("Good job!", response.data.msg, "success");
+						setTimeout(function () {
+							location.reload();
+						}, 1500)
+					} else if (response.data.status == 'warning') {
+						swal({
+							title: "Whoops!",
+							text: response.data.msg,
+							icon: "warning",
+							dangerMode: true,
+						})
+					}
+				})
+				.catch(err => {
+					if (err.response.status == 200) {
+						swal("Good job!", err.response.data.msg, "success");
+						setTimeout(function () {
+							location.reload();
+						}, 1500)
+					} else {
+						swal("Woops!", "Qualcosa è andato storto!", "error");
+						swal.stopLoading();
+						swal.close();
+					}
+				})
+		}
+		swal.stopLoading();
+		swal.close();
+
+	});
+});
+$(document).on('click', '.update-btn', function (e) {
+	e.preventDefault();
+	let actionurl = $(this).data('action');
+	axios.patch(actionurl)
+		.then(response => {
+			if (response.data.status == 'success') {
+				swal("Good job!", response.data.msg, "success");
+				setTimeout(function () {
+					location.reload();
+				}, 1500)
+			} else if (response.data.status == 'unauthorized') {
+				swal("Woops!", response.data.msg, "error");
+			} else {
+				swal("Woops!", "Qualcosa è andato storto!", "error");
+				swal.stopLoading();
+				swal.close();
+			}
+
+		})
+		.catch(err => {
+			if (err.response.status == 200) {
+				swal("Good job!", err.response.data.msg, "success");
+				setTimeout(function () {
+					location.reload();
+				}, 1500)
+			} else {
+				swal("Woops!", "Qualcosa è andato storto!", "error");
+				swal.stopLoading();
+				swal.close();
+			}
+		})
+
+});
+
+$(document).on('click', '.post-action', function (e) {
+	e.preventDefault();
+	let actionurl = $(this).data('action');
+	let msg = $(this).data('content');
+	swal({
+		title: "Sei sicuro?",
+		text: msg,
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+	}).then((val) => {
+		if (val) {
+			axios.post(actionurl)
+				.then(response => {
+					if (response.data.status == 'success') {
+						swal("Good job!", response.data.msg, "success");
+						setTimeout(function () {
+							location.reload();
+						}, 1500)
+					}
+				})
+				.catch(err => {
+					if (err.response.status == 200) {
+						swal("Good job!", err.response.data.msg, "success");
+						setTimeout(function () {
+							location.reload();
+						}, 1500)
+					} else {
+						swal("Woops!", "Qualcosa è andato storto!", "error");
+						swal.stopLoading();
+						swal.close();
+					}
+				})
+		}
+
+	})
+});
+
+$(document).on('click', '.sender-email', function (e) {
+	e.preventDefault();
+	let actionurl = $(this).data('action');
+	$('#email-settings').toggleClass('open')
+	$('#sendemailform').attr('action', actionurl);
+});
---
+++ public/js/config.js
@@ -0,0 +1,14 @@
+$('.summernote').summernote({
+    height: 150,
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['fontname', ['fontname']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link']],
+        ['view', ['fullscreen', 'codeview', 'help']],
+    ],
+
+});
